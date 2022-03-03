3f41364a307b9194d4edc8c2a2890ea4c51195b6
caroline <carzhang96@gmail.com>
2022-03-03 18:04:09 -0500

    update event_manager.rb to print personal form letter

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -42,5 +42,8 @@ contents.each do |row|
 
   legislators = legislators_by_zipcode(zipcode)
 
-  puts "#{name} #{zipcode} #{legislators}"
+  personal_letter = template_letter.gsub('FIRST_NAME', name)
+  personal_letter.gsub!('LEGISLATORS', legislators)
+
+  puts personal_letter
 end
